035293e17ec1fb323517703ac33725878333745b
Sudip-Naskar <sudipdev09@gmail.com>
2023-08-15 23:55:16 +0530

    Done

--- app.js
+++ app.js
@@ -2,8 +2,29 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-console.log(outputDiv.innerHTML);
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input;
+}
+
+function errorHandler(error) {
+    console.log("error occured! " + error)
+    alert("Something wrong with server. Please try again after sometime.")
+}
 
 btnTranslate.addEventListener("click", function clickEventHandler() {
-    outputDiv.innerText = "ajajajajajaj "+ txtInput.value;
+    // outputDiv.innerText = "ajajajajajaj "+ txtInput.value;
+    var inputText = txtInput.value;
+
+    //Calling server for processing
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
 })
